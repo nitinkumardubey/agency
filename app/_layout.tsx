77ace789ab6c31bc3f82ccd0d767cbdfee042fac
Nitin Dubey <nitindubey@Nitins-MacBook-Air.local>
2025-12-07 15:46:37 +0530

    initial changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 export default function Home() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello World how are you?</Text>
+      <Text>Hello World how are you</Text>
     </View>
   );
 }
